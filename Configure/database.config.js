b3f3e2454575eebafd05ecee8fc5dcad9f72d8d3
triquetrx <zdkhan0786@gmail.com>
2023-06-21 11:22:38 +0530

    changed_db_to_dynamo

--- Configure/database.config.js
+++ Configure/database.config.js
@@ -1,20 +1,31 @@
 const mongo = require("mongoose");
 const { logger } = require("./logger.config");
+const dynamoose = require('dynamoose');
 
-let mongoConnectUrl;
-if (process.env.ENV === "development") {
-  mongoConnectUrl = `${process.env.DB_URL}/muvi`;
-} else if (process.env.ENV === "test") {
-  mongoConnectUrl = `${process.env.DB_TEST_URL}`;
-}
+// let mongoConnectUrl;
+// if (process.env.ENV === "development") {
+//   mongoConnectUrl = `${process.env.DB_URL}/muvi`;
+// } else if (process.env.ENV === "test") {
+//   mongoConnectUrl = `${process.env.DB_TEST_URL}`;
+// }
 
-mongo.set("strictQuery", true);
-mongo
-  .connect(mongoConnectUrl)
-  .then(() =>
-    logger.info(
-      `Connected to the database successfully, URL: ${mongoConnectUrl}`
-    )
-  );
+const ddb = new dynamoose.aws.ddb.DynamoDB({
+  "credentials": {
+      "accessKeyId": env.AWS_ACCESS_KEY_ID,
+      "secretAccessKey": env.AWS_SECRET_ACCESS_KEY
+  },
+  "region": AWS_REGION
+});
 
-module.exports.mongo = mongo;
+dynamoose.aws.ddb.set(ddb);
+
+// mongo.set("strictQuery", true);
+// mongo
+//   .connect(mongoConnectUrl)
+//   .then(() =>
+//     logger.info(
+//       `Connected to the database successfully, URL: ${mongoConnectUrl}`
+//     )
+//   );
+
+module.exports.dynamoose = dynamoose;
